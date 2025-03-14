50d44fec4ad6435c874d7a9e75a1d4cd60f1b9d7
Vinicius Perrone <perronevinicius2018@gmail.com>
2025-03-14 19:30:20 -0300

    chore: installed colorama

--- scraping_linkedin.py
+++ scraping_linkedin.py
@@ -2,8 +2,15 @@ from selenium import webdriver
 import time
 
 
+link_linkedin = "https://www.linkedin.com/feed/"
+
 def open_edge_navigator():
     navigator = webdriver.Edge()
+
+    navigator.get(link_linkedin)
+
+    navigator.maximize_window()
+
     time.sleep(10)
 
 if __name__ == '__main__':
